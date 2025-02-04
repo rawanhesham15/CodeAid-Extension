ce94dfcba89134409515bf1407dc197f73933494
Mariam Ayman <101516694+mariamayman10@users.noreply.github.com>
2025-02-04 23:37:04 +0200

    add FileManager functions

--- backend/src/fileManager/fileManager.js
+++ backend/src/fileManager/fileManager.js
@@ -1,17 +1,93 @@
-class FileManager{
-  createFile(){
+import fs from "fs"
+import path from "path"
 
+class FileManager {
+  /**
+   *
+   * @param {string} dirPath
+   * @param {string} fileName
+   */
+  createFile(dirPath, fileName) {
+    const filePath = path.join(dirPath, fileName);
+    try{
+      if(!fs.existsSync(dirPath)){
+        fs.mkdirSync(dirPath, {recursive: true});
+      }
+      fs.writeFileSync(filePath, "", "utf-8");
+    } catch(error) {
+      console.error(`Error creating file: ${error.message}`)
+    }
   }
-  getProjectContent(path){
+  /**
+   *
+   * @param {string} dirPath
+   * @returns {Object}
+   */
+  getProjectContent(dirPath) {
+    try {
+      const items = fs.readdirSync(dirPath, { withFileTypes: true });
+      let filesData = [];
 
-  }
-  getFileContent(path){
+      for (const item of items) {
+        const fullPath = path.join(dirPath, item.name);
+        if (item.isDirectory()) {
+          filesData = filesData.concat(this.getProjectContent(fullPath)); // Recursively process subdirectories
+        } else {
+          const fileContent = this.getFileContent(fullPath);
+          if (fileContent) {
+            filesData.push(fileContent);
+          }
+        }
+      }
 
+      return filesData;
+    } catch (error) {
+      console.error(`Error reading directory: ${dirPath} - ${error.message}`);
+      return [];
+    }
   }
-  updateFileContent(path, content){
-
+  /**
+   *
+   * @param {string} filePath
+   * @returns {Object|null}
+   */
+  getFileContent(filePath) {
+    try {
+      const content = fs.readFileSync(filePath, "utf8");
+      return { filePath, content };
+    } catch (error) {
+      console.error(`Error reading file: ${error.message}`);
+      return null;
+    }
   }
-  deleteFile(path){
-    
+  /**
+   *
+   * @param {string} filePath
+   * @param {string} content
+   */
+  updateFileContent(filePath, content) {
+    try {
+      fs.writeFileSync(filePath, content, "utf8");
+    } catch (error) {
+      console.error(`Error updating file: ${error.message}`);
+    }
+  }
+  /**
+   *
+   * @param {string} filePath
+   */
+  deleteFile(filePath) {
+    try {
+      if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+        console.log(`File deleted: ${filePath}`);
+      } else {
+        console.error("File does not exist.");
+      }
+    } catch (error) {
+      console.error(`Error deleting file: ${error.message}`);
+    }
   }
 }
+
+module.exports = FileManager;
